11f670c6aaa8fce44574e627be576bed3de89f61
SWAT21314 <125081012+SWAT21314@users.noreply.github.com>
2023-03-03 18:49:36 +0000

    Add files via upload

---
+++ pro-c9-project-solution-main/pro-c9-project-solution-main/sketch.js
@@ -0,0 +1,40 @@
+var box;
+
+function setup () {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+Function draw()
+{
+
+
+  If (key IsDown(RIGHT_ARROW))
+  {
+    background("red");
+  }
+
+  If (keyIsDown(LEFT_ARROW))
+  {
+    background("blue")
+  
+  }
+
+  If (keyIsDown(Up_ARROW))
+  {
+    background("yellow")
+
+  }
+
+  IDBKeyRange(KeyIsDown(Down_ARROW))
+  {
+    background("green");
+  }
+
+  drawSprites();
+  }
+
+
+
